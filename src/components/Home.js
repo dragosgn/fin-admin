f27e35cafb0e0fd0c1a87819997db894cef95eb9
Dragos Nedelcu <dragosgn@gmail.com>
2018-02-15 10:58:08 +0100

    boilerplate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,10 @@
 import React from 'react'
+import styled from "styled-components"
+
+const Root = styled.div `
+  padding: 3rem;
+`
+
+export default() => (<Root>
+  <h1>This is home</h1>
+</Root>)
