8b8b59264587dda10581ae9ae880c5e34006a5d6
Dragos Nedelcu <dragosgn@gmail.com>
2018-02-15 11:14:33 +0100

    created reducer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import {Provider} from "react-redux"
 import {ThemeProvider} from 'styled-components'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
 
 
 import App from "./components/App"
 
 // theming at general level
 const theme = {
-
 }
+
+
+const rootReducer = combineReducers({
+
+})
+
+
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware
+))
 
 
 const renderApp = (Component) => {
   <ThemeProvider theme={theme}>
-    <Provider>
+    <Provider store={store}>
       <Component />
     </Provider>
   </ThemeProvider>, document.getElementById("app")
